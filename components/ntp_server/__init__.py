c7c7a521e89915bdf5906359fc591a85886d8e31
gdave44 <gdave44@gmail.com>
2026-03-07 08:32:25 -0700

    Update __init__.py

--- components/ntp_server/__init__.py
+++ components/ntp_server/__init__.py
@@ -1,5 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
+from esphome.core import CORE
 from esphome.const import CONF_ID, CONF_TIME
 
 CODEOWNERS = ["@RobertJN64"]
@@ -18,5 +19,8 @@ CONFIG_SCHEMA = cv.All(
 )
 
 async def to_code(config):
+    if CORE.is_esp32 and CORE.using_arduino:
+        cg.add_library("Ethernet", None)
+        cg.add_library("WiFi", None)
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
